60c2da5abb98d8aee23d2ff17449624c135c865d
andreiananko <110332587+andreiananko@users.noreply.github.com>
2024-01-26 01:14:05 +0300

    Update index.js
    fix: remove using synchronous methods of the fs module

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,31 +1,30 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const sourceFolder = '04-copy-directory/files';
 const destinationFolder = '04-copy-directory/files-copy';
 
-function copyDir(source, destination) {
-  fs.readdir(source, { withFileTypes: true }, (err, files) => {
-    if (err) {
-      console.error('Error reading directory:', err);
-      return;
+async function copyDir(source, destination) {
+  try {
+    const files = await fs.readdir(source, { withFileTypes: true });
+
+    if (!await fs.access(destination).then(() => true).catch(() => false)) {
+      await fs.mkdir(destination);
     }
 
-    if (!fs.existsSync(destination)) {
-      fs.mkdirSync(destination);
-    }
-
-    files.forEach((file) => {
+    for (const file of files) {
       const sourcePath = path.join(source, file.name);
       const destinationPath = path.join(destination, file.name);
 
       if (file.isDirectory()) {
-        copyDir(sourcePath, destinationPath);
+        await copyDir(sourcePath, destinationPath);
       } else {
-        fs.copyFileSync(sourcePath, destinationPath);
+        await fs.copyFile(sourcePath, destinationPath);
       }
-    });
-  });
+    }
+  } catch (err) {
+    console.error('Error copying directory:', err);
+  }
 }
 
 copyDir(sourceFolder, destinationFolder);
